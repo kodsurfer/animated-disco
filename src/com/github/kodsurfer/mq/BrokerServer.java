9d546559eb7a10ead87d1e92ecac809845500cc7
kodsurfer <kodi2020@yandex.com>
2023-06-29 17:27:34 +0300

    add ctor to BrokerServer

--- src/com/github/kodsurfer/mq/BrokerServer.java
+++ src/com/github/kodsurfer/mq/BrokerServer.java
@@ -1,6 +1,16 @@
 package com.github.kodsurfer.mq;
 
+import java.net.Socket;
+
 public class BrokerServer implements Runnable {
+
+    public static final int PORT = 9999;
+    private final Socket socket;
+
+    public BrokerServer(Socket socket) {
+        this.socket = socket;
+    }
+
     @Override
     public void run() {
 

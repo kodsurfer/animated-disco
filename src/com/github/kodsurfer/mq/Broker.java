ea84872dc5c482fd4631904e15fae78aa29ecdce
kodsurfer <kodi2020@yandex.com>
2023-06-29 17:11:41 +0300

    add consume method to Broker

--- src/com/github/kodsurfer/mq/Broker.java
+++ src/com/github/kodsurfer/mq/Broker.java
@@ -6,11 +6,20 @@ public class Broker {
     private final static int SIZE_MAX = 10;
     private static ArrayBlockingQueue<String> mq = new ArrayBlockingQueue<>(SIZE_MAX);
 
-
     public static void produce(String msg) {
         if (mq.offer(msg))
             System.out.println("Send message to message broker: " + msg + " current cache: " + mq.size());
         else
             System.out.println("Message can't be send");
+    }
+
+    public static String consume() {
+        String msg = mq.poll();
+        if (msg != null)
+            System.out.println("Message: " + msg + " already stored in message broker: " + mq.size());
+        else
+            System.out.println("There are no messages in message broker");
+
+        return msg;
     }
 }
